be2fd607f9942e34732c373ddd4e95726ae5effa
joblessmf <ndimitrov992@gmail.com>
2025-06-20 14:22:03 +0300

    Initial Commit

---
+++ Habit Track/Habit_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackApp.swift
+//  Habit Track
+//
+//  Created by Nikolay Simeonov on 20.06.25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
